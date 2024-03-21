77d3e68d3653afa60091ac92acaa2a149be6b9d3
Erik Hrazdira <erik.hrazdira@tul.cz>
2024-03-21 13:16:15 +0100

    Added all files

--- src/test/java/cz/tul/sti2024/cv/controller/PaymentControllerTest.java
+++ src/test/java/cz/tul/sti2024/cv/controller/PaymentControllerTest.java
@@ -10,17 +10,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.hamcrest.Matchers.equalTo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.core.io.ClassPathResource;
+import java.io.IOException;
+import java.nio.file.Files;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class HelloWorldControllerTest {
+public class PaymentControllerTest {
     @Autowired
     private MockMvc mvc;
 
     @Test
-    public void getHello() throws Exception {
+    void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Hello world")));
     }
+    @Test
+    void testPayment() throws IOException {
+        PaymentController paymentController = new PaymentController();
+        String payload = Files.readString(new ClassPathResource("inputdata/input_valid.json").getFile().toPath());
+        paymentController.paymentProcesing(payload);
+        Assertions.assertEquals(10, 10);
+    }
+
 }
